13ce341c6c01b6b29e6889f3a7835e660a93bb64
teodor-sakal-franciskovic <sakal.teodor@gmail.com>
2024-05-12 13:48:15 +0200

    fix: remove basepath from config

--- nextjs-minikube-integration/next.config.mjs
+++ nextjs-minikube-integration/next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'standalone', 
-    // Ovde sam isprobavao sve i svašta (i buildovao svaki put), bezuspešno.
-    assetPrefix: 'https://www.codemonkeys.com/frontend',
-    basePath: '/frontend'
+    output: 'standalone',
 };
 
 export default nextConfig;
