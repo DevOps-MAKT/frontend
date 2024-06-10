1af0cb420741948e6c0bfc1f714b2c0b7e92354c
anaandjelic <ana.andjelic161@gmail.com>
2024-06-10 15:09:58 +0200

    fix: add consistent icons

--- nextjs-minikube-integration/src/icons/house.jsx
+++ nextjs-minikube-integration/src/icons/house.jsx
@@ -1,16 +1,18 @@
 export const HouseIcon = (props) => (
   <svg
+    class="w-7 h-7 text-white"
+    aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
     fill="none"
     viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="size-6"
-    {...props}
   >
     <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
       d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5"
     />
   </svg>
--- nextjs-minikube-integration/src/icons/logIn.jsx
+++ nextjs-minikube-integration/src/icons/logIn.jsx
@@ -1,16 +1,18 @@
 export const LogInIcon = (props) => (
   <svg
+    class="w-7 h-7 text-white"
+    aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
     fill="none"
     viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="size-6"
-    {...props}
   >
     <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
       d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"
     />
   </svg>
